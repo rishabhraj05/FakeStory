125ace24c0d56352c114cf3001fc652e40d761a9
rishabhraj <rishabh05raj@gmail.com>
2025-02-13 11:57:59 +0530

    1st Project, Creation

--- src/main/java/com/dev/fakestory/FakeStory/Controller/ProductController.java
+++ src/main/java/com/dev/fakestory/FakeStory/Controller/ProductController.java
@@ -5,6 +5,7 @@ import com.dev.fakestory.FakeStory.FakeStoryDTO.FakeStoryProductResponse;
 import com.dev.fakestory.FakeStory.dto.ProductRequest;
 import com.dev.fakestory.FakeStory.dto.ProductResponse;
 import com.dev.fakestory.FakeStory.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,10 +29,16 @@ public class ProductController
     }
 
     @GetMapping("/getAllProducts")
-    public ResponseEntity<List<FakeStoryProductResponse>> getAllProducts()
+    public ResponseEntity<List<FakeStoryProductResponse>> getAllProducts(HttpServletRequest request)
     {
         List<FakeStoryProductResponse> productResponses =
                 productService.getAllProducts();
+
+        String clientIp = request.getHeader("TRUE_CLIENT_IP");
+
+        if (clientIp == null || clientIp.isEmpty()) {
+            clientIp = request.getRemoteAddr();
+        }
 
         return new ResponseEntity<>(productResponses, HttpStatus.OK);
     }
@@ -67,5 +74,35 @@ public class ProductController
     public ResponseEntity<FakeStoryProductRequest> addProduct(@RequestBody FakeStoryProductRequest productRequest)
     {
        return productService.addProduct(productRequest);
+    }
+
+    @GetMapping("/checkIP")
+    public String checkIP(HttpServletRequest request) {
+        String ip = null;
+
+        // Check headers in order for the client's IP
+        if ((ip = request.getHeader("HTTP_CLIENT_IP")) != null && !ip.isEmpty()) {
+            return ip;
+        }
+        if ((ip = request.getHeader("HTTP_X_FORWARDED_FOR")) != null && !ip.isEmpty()) {
+            return ip;
+        }
+        if ((ip = request.getHeader("HTTP_X_FORWARDED")) != null && !ip.isEmpty()) {
+            return ip;
+        }
+        if ((ip = request.getHeader("HTTP_FORWARDED_FOR")) != null && !ip.isEmpty()) {
+            return ip;
+        }
+        if ((ip = request.getHeader("HTTP_FORWARDED")) != null && !ip.isEmpty()) {
+            return ip;
+        }
+        if ((ip = request.getHeader("REMOTE_ADDR")) != null && !ip.isEmpty()) {
+            return ip;
+        }
+        if ((ip = request.getRemoteAddr()) != null && !ip.isEmpty()) {
+            return ip;
+        }
+
+        return "IP not found";
     }
 }
